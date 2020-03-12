ad5d5208d8c71b02f5385ded31ad8692a2684167
Wayne Porter <wporter82@gmail.com>
2020-03-11 19:42:34 -0700

    Fix autogen to test for automake

--- autogen.sh
+++ autogen.sh
@@ -4,9 +4,15 @@
 	echo "You must have autoreconf installed."
 	DIE=1
 }
+(automake --version) < /dev/null > /dev/null || {
+	echo "You must have automake installed to generate procps-ng build system."
+	DIE=1
+}
 
 echo "Generating build-system by:"
 echo "    autoreconf: $(autoreconf --version | head -1)"
+echo "    aclocal: $(aclocal --version | head -1)"
+echo "    automake: $(automake --version | head -1)"
 
 autoreconf --install
 
